1fc3729babcd054c8063a0e88cf05560b7fbc248
Sokrat Poghosyan <Sokrat_Poghosyan@epam.com>
2021-01-28 20:49:24 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/S-Solo/json-server-test/users')
+      .then(res => res.json())
+      .then(resJson => {
+        setUsers(resJson);
+      })
+      .catch(err => {
+        console.log('err: ', err);
+      })
+
+
+    fetch('https://my-json-server.typicode.com/S-Solo/json-server-test/users', {
+      method: 'POST',
+      body: JSON.stringify([
+        {
+          id: 1,
+          "name": "John",
+          "surname": "Doe",
+          "age": 43
+        },
+        {
+          id: 2,
+          "name": "Johny",
+          "surname": "Doe",
+          "age": 50
+        },
+        {
+          id: 3,
+          "name": "Johan",
+          "surname": "Doe",
+          "age": 81
+        }
+      ])
+    })
+      .then(res => res.json())
+      .then(resJson => {
+        console.log('resJson: ', resJson);
+      })
+      .catch(err => {
+        console.log('err: ', err);
+      })
+  }, [])
+
+
   return (
     <div className="App">
-      Heeellllooo
+      {users && users.map(el => {
+        return <span key={el.name}>el.name</span>
+      })}
     </div>
   );
 }
